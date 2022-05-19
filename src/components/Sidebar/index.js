9cfbf8023debcc000b19d69c8976043e8cfc2315
francioniniccolo <niccolo.francioni@studenti.unicam.it>
2022-05-20 00:01:00 +0200

    F

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+import './index.scss'
+
+const Sidebar = () => {
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+        <img src={}></img>
+    </Link>
+  </div>
+}
+
+export default Sidebar
